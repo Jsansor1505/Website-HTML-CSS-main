7ccca351fbda040e75cbcf38ac60d4a94d3ba0e0
“jsansor1505” <jsansor1505@g.educaand.es>
2025-12-06 17:33:50 +0100

    Commit final

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,19 +23,58 @@ const linkAction = () => {
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR HEADER ===============*/
-const blurHeader = () =>{
+const blurHeader = () => {
     const header = document.getElementById('header')
     // Add a class if the bottom offset is greater than 50 of the viewport
-    this.scrollY >= 50 ? header.classList.add('blur-header') 
-                       : header.classList.remove('blur-header')
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+        : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
 
 /*=============== SHOW SCROLL UP ===============*/
-
-
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+const scrollActive = () => {
+    const scrollDown = window.scrollY
 
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '40px',
+    opacity: 1,
+    scale: 1.1,
+    duration: 2500,
+    delay: 300,
+    // reset: true, // Animations repeat
+})
+sr.reveal('.home___data, .about__img, .about__data, .visit__data')
+sr.reveal('.home___image, .footer__img-1, .footer__img-2', {rotate: {z: -15} })
+sr.reveal('.home___bread, .about__bread', {rotate: {z: 15} })
+sr.reveal('.home___footer', {scale: 1, origin: 'bottom'})
+sr.reveal('.new__card:nth-child(1) img', {rotate: {z: -30}, distance: 0 })
+sr.reveal('.new__card:nth-child(2) img', {rotate: {z: 15}, distance: 0, delay: 600 })
+sr.reveal('.new__card:nth-child(3) img', {rotate: {z: -30}, distance: 0, delay: 900 })
+sr.reveal('.favourite__card img', {interval: 100, rotate: {z: 15}, distance: 0})
+sr.reveal('.footer__container', {scale: 1})
